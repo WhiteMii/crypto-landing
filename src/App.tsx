6cc1cbeb74ed897502ab752aad75931336544f95
Yaroslav Yaroshovets <white240403@gmail.com>
2023-03-19 18:36:32 +0200

    all packages set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div className="flex font-bold">Working</div>;
+  return <div className=""></div>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,28 @@ module.exports = {
   theme: {
     extend: {},
   },
+  theme: {
+    extend: {
+      colors: {
+        "primary-500": "#f3f2f4",
+        "secondary-500": "linear-gradient(25deg, #2600fc, #ff00ea)",
+        "bg-500": "#0f051d",
+        "green-500": "rgb(14, 203, 129)",
+      },
+
+      fontFamily: {
+        headingFont: ["Space Grotesk", "sans-serif"],
+        commonFont: ["Noto Sans Georgian", "sans-serif"],
+      },
+      content: {
+        evolvetext: "url('./assets/EvolveText.png')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
+  },
   plugins: [],
 };
